a30987fe13f980c4ad51be37c8a79b3861e43564
Vincent <vshan0816@gmail.com>
2022-01-16 18:57:39 -0500

    create header, home, add routes to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
 
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {Navbar} from './Navbar'
+import {Header} from './Header'
+import {CharacterForm} from './CharacterForm'
+import {ClassCard} from './ClassCard'
+import {ClassList} from './ClassList'
+import {Home} from './Home'
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar />
+        <Header />
+        <Switch>
+          <Route path ="/characters/new">
+            <CharacterForm />
+          </Route>
+
+          <Route path ="/characters/:id">
+            <ClassCard />
+          </Route>
+
+          <Route path ="/characters/">
+            <ClassList />
+          </Route>
+
+          <Route path ="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
