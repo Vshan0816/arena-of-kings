2978938d2c32c9e82a8c2cff64e0f291f38a9e9d
Vincent <vshan0816@gmail.com>
2022-01-24 15:21:54 -0500

    add fetch GET for characters container

--- src/containers/CharactersContainer.js
+++ src/containers/CharactersContainer.js
@@ -1,9 +1,22 @@
-
+import {useState, useEffect} from 'react'
+import { ClassList } from '../components/ClassList'
 
 export const CharactersContainer = () => {
+    
+    const [classes, setClasses] = useState([])
+    const fetchData = () => {
+        fetch("http://localhost:3001/classes")
+        .then(response => response.json())
+        .then(data => setClasses(data))
+        .catch(error => alert(error))
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
     return (
         <div>
-            
+            <ClassList />
         </div>
     )
 }
