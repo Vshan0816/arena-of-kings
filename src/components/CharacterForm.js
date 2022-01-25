b1afbd9293d38d92f72343d5b9b9997efd2fb089
Vincent <vshan0816@gmail.com>
2022-01-25 01:22:49 -0500

    create filter function for select

--- src/components/CharacterForm.js
+++ src/components/CharacterForm.js
@@ -3,14 +3,25 @@ import {useHistory} from 'react-router-dom'
 
 export const CharacterForm = ({classes}) => {
     const [type, setType] = useState("")
+    const [filteredSkills, setFilteredSkills] = useState(classes)
     const handleSubmit = (e) => {
         e.preventDefault()
     }
-    const skillMap = classes.map(classs=>{ 
-        return classs.skills.map(skill=>{
-            return console.log(skill)    
-        })
-    })
+    // const skillMap = classes.map(classs=>{ 
+    //     return classs.skills.map(skill=>{
+              
+    //     })
+    // })
+
+    const handleSelect = (selectValue) => {
+        console.log(selectValue)
+        const filteredClasses = classes.filter(classs=> classs.class===selectValue)
+        setFilteredSkills(filteredClasses)
+        console.log(filteredClasses)
+        
+
+    }
+    console.log(filteredSkills)
     return (
         <form onSubmit={handleSubmit}>
             <h1>Create a Character</h1>
@@ -18,20 +29,22 @@ export const CharacterForm = ({classes}) => {
             <select 
                 name="classes"
                 id="class-select"
-                onChange={e => setType(e.target.value)}>
+                onChange={e => {
+                    handleSelect(e.target.value)}}>
                 <option value="">Please Select a Class</option>
-                <option value="assassin">Assassin</option>
-                <option value="champion">Champion</option>
-                <option value="elder">Elder</option>
-                <option value="lich">Lich</option>
-                <option value="mystic">Mystic</option>
-                <option value="nihilist">Nihilist</option>
-                <option value="paladin">Paladin</option>
-                <option value="ranger">Ranger</option>
-                <option value="scholar">Scholar</option>
-                <option value="wizard">Wizard</option>
+                <option value="Assassin">Assassin</option>
+                <option value="Champion">Champion</option>
+                <option value="Elder">Elder</option>
+                <option value="Lich">Lich</option>
+                <option value="Mystic">Mystic</option>
+                <option value="Nihilist">Nihilist</option>
+                <option value="Paladin">Paladin</option>
+                <option value="Ranger">Ranger</option>
+                <option value="Scholar">Scholar</option>
+                <option value="Wizard">Wizard</option>
             </select>
-
+            {}
+        
             
         </form>
     )
