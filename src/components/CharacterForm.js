83caf895d600a39f090907ee7d3ced57a4b3e1f3
Vincent <vshan0816@gmail.com>
2022-01-24 23:12:37 -0500

    start option value for character form

--- src/components/CharacterForm.js
+++ src/components/CharacterForm.js
@@ -1,8 +1,29 @@
+import {useState} from 'react'
+import {useHistory} from 'react-router-dom'
 
 export const CharacterForm = () => {
+    const [type, setType] = useState("")
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
     return (
-        <div>
-            
-        </div>
+        <form onSubmit={handleSubmit}>
+            <h1>Create a Character</h1>
+            <label for="class-select">Select a Class:</label>
+            <select name="classes" id="class-select">
+            <input onChange={e => setType(e.target.value)}/>
+                <option value="">Please Select a Class</option>
+                <option value="assassin">Assassin</option>
+                <option value="champion">Champion</option>
+                <option value="elder">Elder</option>
+                <option value="lich">Lich</option>
+                <option value="mystic">Mystic</option>
+                <option value="nihilist">Nihilist</option>
+                <option value="paladin">Paladin</option>
+                <option value="ranger">Ranger</option>
+                <option value="scholar">Scholar</option>
+                <option value="wizard">Wizard</option>
+            </select>
+        </form>
     )
 }
