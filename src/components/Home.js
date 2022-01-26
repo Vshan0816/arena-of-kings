4a313cc30461baf91c2ba6a96e0a0e946bdd5331
Vincent <vshan0816@gmail.com>
2022-01-25 21:49:20 -0500

    add Arena of Kings logo, update Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,13 @@
 export const Home = () => {
     return (
         <div>
-            
+            <img src="../Images/AoKLogo.png" alt="AoK Logo" />
+            <h3>Description:</h3>
+            <p>Arena Of Kings is a 3v3 multiplayer game on Steam developed by Villain Games</p>
+            <p>Click on Classes learn about the different classes</p>
+            <p>In the Classes page, click see skills to learn about the skills of the chosen Class</p>
+            <p>You can create your own characters under the New Character form</p>
+            <p>Your characters will be displayed on the Your Characters page</p>
         </div>
     )
 }
