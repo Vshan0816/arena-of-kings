d09c51dafafa87208f37868bd41d786b99cbe34b
Vincent <vshan0816@gmail.com>
2022-01-25 19:52:05 -0500

    add more icons to CharacterCard

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -2,23 +2,52 @@ import React from 'react';
 
 export const CharacterCard = ({character, classes}) => {
     
-  const superFind = (skillName) => {
-     return   classes
-        .find(classs => character.classType === classs.class)
-        .skills
-        .find(skill => character[skillName] === skill.skillName)
-        .icon
+  const findSkillImage = (skillName) => {
+     return classes
+            .find(classs => character.classType === classs.class)
+            .skills
+            .find(skill => character[skillName] === skill.skillName)
+            .icon
         
   }  
 
   return (
-    <div>
+    <div style={{border:"solid", width: "17 %", height:"25%", margin:"auto"}}>
         <p>{character.name}</p>
         {character && classes.length > 0
-        ?  <div> 
-                {character.skill1}
-                
-                <img src={`../${superFind("skill1")}`} />
+        ? <div> 
+              <div>
+                {character.skill1}<br/>
+                <img src={`../${findSkillImage("skill1")}`} alt={character.id} />
+              </div>
+              <div>
+                {character.skill2}<br/>
+                <img src={`../${findSkillImage("skill2")}`} alt={character.id} />
+              </div> 
+              <div>
+                {character.skill3}<br/>
+                <img src={`../${findSkillImage("skill3")}`} alt={character.id} />
+              </div>
+              <div>
+                {character.skill4}<br/>
+                <img src={`../${findSkillImage("skill4")}`} alt={character.id} />
+              </div>
+              <div>
+                {character.skill5}<br/>
+                <img src={`../${findSkillImage("skill5")}`} alt={character.id} />
+              </div>
+              <div>
+                {character.skill6}<br/>
+                <img src={`../${findSkillImage("skill6")}`} alt={character.id} />
+              </div>
+              <div>
+                {character.skill7}<br/>
+                <img src={`../${findSkillImage("skill7")}`} alt={character.id} />
+              </div>
+              <div>
+                {character.skill8}<br/>
+                <img src={`../${findSkillImage("skill8")}`} alt={character.id} />
+              </div>
            </div>
         :null}
     </div>
